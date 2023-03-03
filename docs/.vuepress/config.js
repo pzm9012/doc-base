363f9b01bdd01451535fec99dbb1627707e55922
pzm9012 <pzm9012@outlook.com>
2023-03-03 10:34:29 +0800

    1

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'pzm9012 的文稿',
+    description: 'pzm9012 的文稿',
+    base: '/doc/',
+    themeConfig: {
+        logo: '',
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '语雀页面', link: 'https://yuque.com/pzm9012/ct5ume/' },
+          ]
+      }
+  }
