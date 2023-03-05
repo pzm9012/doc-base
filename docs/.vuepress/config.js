2d99785a29bb072cac3440e93b416d6b39f6092a
pzm9012 <pzm9012@outlook.com>
2023-03-05 12:05:44 +0800

    2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,25 @@ module.exports = {
         logo: '',
         nav: [
             { text: '首页', link: '/' },
-            { text: '语雀页面', link: 'https://yuque.com/pzm9012/ct5ume/' },
-          ]
+            { text: '语雀', link: 'https://yuque.com/pzm9012/ct5ume/' },
+          ],
+        sidebar: 
+        [
+         {
+            title: 'Group 1',   // 必要的
+            path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+            collapsable: false, // 可选的, 默认值是 true,
+            sidebarDepth: 1,    // 可选的, 默认值是 1
+            children: [
+               '/'
+            ]
+        },
+        {
+            title: 'Group 2',
+            children: [ /* ... */ ],
+            initialOpenGroupIndex: -1 // 可选的, 默认值是 0
+        }
+                                                                                                                      
+        ]
       }
   }
